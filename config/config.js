8aff506fc7b9e35086898f833f65b359b4816fe0
Enrique Gimenez <fenriquegimenez@gmail.com>
2021-01-10 16:31:01 -0300

    mongoDB setted up

--- config/config.js
+++ config/config.js
@@ -1,14 +1,11 @@
 module.exports = {
   dev: process.env.NODE_ENV !== "production",
   port: process.env.PORT || 3000,
-  error: {
-    ok: 200,
-    created: 201,
-    badRequest: 400,
-    forbidden: 403,
-    notFound: 404,
-    internalError: 500,
-  },
+  cors: process.env.CORS,
+  dbUser: process.env.DB_USER,
+  dbPassword: process.env.DB_PASSWORD,
+  dbHost: process.env.DB_HOST,
+  dbName: process.env.DB_NAME,
   challenges: {
     port: process.env.PORT || 8000,
   },
